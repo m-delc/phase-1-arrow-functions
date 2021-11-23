a299e1ff2182ca80337b0e881c9ae9b0be064b07
MichaelDelcambre <delcambrem@gmail.com>
2021-11-23 09:45:08 -0600

    commit tree b6cee373e21073a3f62e2f26d2d72ba467f4af71

---
+++ index.js
@@ -0,0 +1,14 @@
+// const divide = (num1, num2) => num1 / num2;
+
+// divide(2000,100);
+
+const divide = () => {
+    return 2000/100;
+}
+
+// const square = () => {}
+
+const square = x => x * x
+
+// const add = () => {}
+const add = (param1, param2) => param1 + param2;
