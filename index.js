fcded79a44148c52502487d2f321df64e7adb831
MichaelDelcambre <delcambrem@gmail.com>
2021-11-23 09:45:33 -0600

    done by mmd

---
+++ index.js
@@ -0,0 +1,11 @@
+// const divide = (num1, num2) => num1 / num2;
+
+// divide(2000,100);
+
+const divide = () => {
+    return 2000/100;
+}
+
+const square = x => x * x
+
+const add = (param1, param2) => param1 + param2;
